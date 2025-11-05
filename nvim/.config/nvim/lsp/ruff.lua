12d0283951d7ba654b83daac8ea44b9f33a04c31
Jorge Gomez <jgomezmail-dev@proton.me>
2025-11-05 18:36:04 -0500

    applied stylua on entire folder

--- nvim/.config/nvim/lsp/ruff.lua
+++ nvim/.config/nvim/lsp/ruff.lua
@@ -1,15 +1,16 @@
 return {
-    cmd = {
-        "ruff", "server"
-    },
-    filetypes = {
-        "python",
-    },
-    root_markers = {
-        ".git",
-        "pyproject.toml",
-        ".venv",
-    },
-    single_file_support = true,
-    log_level = vim.lsp.protocol.MessageType.Warning,
+	cmd = {
+		"ruff",
+		"server",
+	},
+	filetypes = {
+		"python",
+	},
+	root_markers = {
+		".git",
+		"pyproject.toml",
+		".venv",
+	},
+	single_file_support = true,
+	log_level = vim.lsp.protocol.MessageType.Warning,
 }
