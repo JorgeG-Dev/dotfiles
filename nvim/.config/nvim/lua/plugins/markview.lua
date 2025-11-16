d1bd97fad755312dafc641703cc75cc5ea7baad7
Jorge Gomez <jgomezmail-dev@proton.me>
2025-11-16 16:25:33 -0500

    adding keybinds for markdown preview

--- nvim/.config/nvim/lua/plugins/markview.lua
+++ nvim/.config/nvim/lua/plugins/markview.lua
@@ -1,4 +1,8 @@
 return {
 	"OXY2DEV/markview.nvim",
 	lazy = false,
+	keys = {
+		{ "<leader>mt", "<cmd>Markview toggle<cr>", desc = "Toggle Markdown Preview" },
+		{ "<leader>ms", "<cmd>Markview splitToggle<cr>", desc = "Toggle Markdown Splitview" },
+	},
 }
